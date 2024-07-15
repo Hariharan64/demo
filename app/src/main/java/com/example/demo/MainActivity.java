716c55dc200e52bd5f358ec310a70f2a257a37a5
Hariharan64 <hariharan98312@gmail.com>
2024-07-15 08:49:16 +0530

    star

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -8,27 +11,39 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button btnOpenWhatsApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnRequestPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnRequestPermission=findViewById(R.id.btnRequestPermission);
 
-        btnOpenWhatsApp = findViewById(R.id.btnOpenWhatsApp);
-        btnOpenWhatsApp.setOnClickListener(new View.OnClickListener() {
+        btnRequestPermission.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                Intent intent=new Intent(MainActivity.this,LoginPhoneNumberActivity.class);
                 startActivity(intent);
             }
         });
+
+
     }
 }
 
